5323f034a7419b269d0a1bc5be23829a2ce744c5
Paul Booker <paul@paulbooker.co.uk>
2023-10-25 19:25:51 +0100

    udemy-job-board-115

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -11,8 +11,26 @@ class JobController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        return view('jobs.index', ['jobs' => Job::all()]);
+    {	
+	    $jobs = Job::query();
+	    
+	    $jobs->when(request('search'), function ($query) {
+			$query->where(function ($query) {
+				$query->where('title', 'like', '%' . request('search') . '%')
+		    		->orWhere('description', 'like', '%' . request('search') . '%');
+			});
+	    })->when(request('min_salary'), function ($query) {
+		    $query->where('salary', '>=', request('min_salary'));
+	    })->when(request('max_salary'), function ($query) {
+		    $query->where('salary', '<=', request('max_salary'));
+	    })->when(request('experience'), function ($query) {
+		    $query->where('experience', request('experience'));
+	    })->when(request('category'), function ($query) {
+		    $query->where('category', request('category'));
+	    });
+
+	    
+        return view('jobs.index', ['jobs' => $jobs->get()]);
     }
 
     /**
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -17,9 +17,5 @@
 		</div>	
 	</div>
 	
-	<p class="mb-4 text-sm text-slate-500">
-		{!! nl2br(e($job->description)) !!}
-	</p>
-	
 	{{ $slot }}
 </x-card>
